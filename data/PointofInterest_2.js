60383a5bf568f0de91a2c456b46d70d597239beb
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2021-08-13 20:38:38 +0800

    Add files via upload

---
+++ data/PointofInterest_2.js
@@ -0,0 +1,20 @@
+var json_PointofInterest_2 = {
+"type": "FeatureCollection",
+"name": "PointofInterest_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Bubur Ayam Cikini H.R. Suleman", "desc": "Bubur Ayam Cikini H.R. Suleman, Jl. Cisadane No.121, RT.9\/RW.4, Cikini, Kec. Menteng, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta 10330, Indonesia", "X": null, "Y": null, "Info": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 106.8404623, -6.1967696 ] } },
+{ "type": "Feature", "properties": { "name": "Museum Perumusan Naskah Proklamasi", "desc": "Formulation of Proclamation Text Museum, Jl. Imam Bonjol No.1, RW.4, Menteng, Kec. Menteng, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta 10310, Indonesia", "X": null, "Y": null, "Info": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 106.8310089, -6.2003738 ] } },
+{ "type": "Feature", "properties": { "name": "Tugu Proklamasi", "desc": "Jl. Penataran, RT.10\/RW.2, Pegangsaan, Kec. Menteng, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta 10320, Indonesia", "X": null, "Y": null, "Info": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 106.8457241, -6.2029269 ] } },
+{ "type": "Feature", "properties": { "name": "Museum Kebangkitan Nasional", "desc": "National Awakening Museum, Jl. Dr. Abdul Rahman Saleh No.26, RT.4\/RW.5, Senen, Central Jakarta City, Jakarta 10410, Indonesia", "X": null, "Y": null, "Info": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 106.838094, -6.178352 ] } },
+{ "type": "Feature", "properties": { "name": "Monumen Nasional", "desc": "National Monument, Gambir, Central Jakarta City, Jakarta, Indonesia", "X": null, "Y": null, "Info": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 106.8271528, -6.1753924 ] } },
+{ "type": "Feature", "properties": { "name": "Masjid Agung Sunda Kelapa", "desc": null, "X": null, "Y": null, "Info": "Public Facilities" }, "geometry": { "type": "Point", "coordinates": [ 106.832145, -6.201283 ] } },
+{ "type": "Feature", "properties": { "name": "GPIB Paulus Jakarta", "desc": null, "X": null, "Y": null, "Info": "Public Facilities" }, "geometry": { "type": "Point", "coordinates": [ 106.831619, -6.200338 ] } },
+{ "type": "Feature", "properties": { "name": "Rumah Sakit Umum Nasional dr. Cipto Mangunkusumo", "desc": null, "X": null, "Y": null, "Info": "Public Facilities" }, "geometry": { "type": "Point", "coordinates": [ 106.846861, -6.196853 ] } },
+{ "type": "Feature", "properties": { "name": "RSPAD Gatot Soebroto", "desc": null, "X": null, "Y": null, "Info": "Public Facilities" }, "geometry": { "type": "Point", "coordinates": [ 106.836742, -6.176587 ] } },
+{ "type": "Feature", "properties": { "name": "SPBU Pertamina Cikini", "desc": null, "X": null, "Y": null, "Info": "Public Facilities" }, "geometry": { "type": "Point", "coordinates": [ 106.840597, -6.196021 ] } },
+{ "type": "Feature", "properties": { "name": "SPBU Pertamina Kramat Raya", "desc": null, "X": null, "Y": null, "Info": "Public Facilities" }, "geometry": { "type": "Point", "coordinates": [ 106.843589, -6.184924 ] } },
+{ "type": "Feature", "properties": { "name": "Polsek Sawah Besar", "desc": null, "X": null, "Y": null, "Info": "Public Facilities" }, "geometry": { "type": "Point", "coordinates": [ 106.838948, -6.170158 ] } },
+{ "type": "Feature", "properties": { "name": "Polsek Menteng", "desc": null, "X": null, "Y": null, "Info": "Public Facilities" }, "geometry": { "type": "Point", "coordinates": [ 106.838394, -6.19652 ] } }
+]
+}
